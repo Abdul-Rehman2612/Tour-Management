0f679ba90e9c4ee76cf76b59e1479a4a02bab5ae
Abdul-Rehman2612 <aabr2612@gmail.com>
2024-07-20 22:56:02 +0500

    Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../styles/home.css";
-import { Container, Row, Col} from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
 import Subtitle from "./../shared/subtitle";
+
+import SearchBar from "../shared/SearchBar";
+
 const Home = () => {
   return (
     <>
@@ -16,11 +19,37 @@ const Home = () => {
             <Col lg="6">
               <div className="hero__content">
                 <div className="hero__subtitle d-flex align-items-center">
-                  <Subtitle subtitle={'Know Before You Go'}/>
+                  <Subtitle subtitle={"Know Before You Go"} />
                   <img src={worldImg} alt="" />
                 </div>
+                <h1>
+                  Traveling opens the door to creating{" "}
+                  <span className="highlight">memories</span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Itaque explicabo consectetur ab eligendi nihil vel laborum,
+                  accusamus atque, alias iusto saepe ex labore enim!
+                  Voluptatibus adipisci saepe aliquam nostrum minima.
+                </p>
               </div>
             </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-4">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" />
+              </div>
+            </Col>
+            <SearchBar/>
           </Row>
         </Container>
       </section>
