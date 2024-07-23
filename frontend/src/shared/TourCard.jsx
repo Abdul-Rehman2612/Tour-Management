264f64bb36347252f6313593f77b7cb786828972
Abdul-Rehman2612 <aabr2612@gmail.com>
2024-07-23 16:22:09 +0500

    New Changed

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -2,33 +2,41 @@ import React from "react";
 import { Card, CardBody } from "reactstrap";
 import { Link } from "react-router-dom";
 
-const TourCard = ({ tour }) => {
-  const { id, title, city, photo, price, featured, avgRating, reviews } = tour;
+import './tour-card.css';
 
+const TourCard = ({ tour }) => {
+  const { id, title, city, photo, price, featured, reviews } = tour;
+
+  const totalRating = reviews?.reduce((acc,item)=> acc+item.rating,0);
+  const avgRating = 
+  totalRating === 0 
+  ? ''
+  :totalRating===1
+  ? totalRating
+  : totalRating/reviews?.length;
+  
   return (
     <div className="tour__card">
       <Card>
         <div className="tour__img">
           <img src={photo} alt="tour-img" />
-          <span>Featured</span>
+          { featured && <span>Featured</span>}
         </div>
-      </Card>
-
       <CardBody>
         <div className="card__top d-flex align-items-center justify-content-between">
           <span className="tour__location d-flex align-items-center gap-1">
-            <i class="ri-map-pin-line"></i>
+            <i className="ri-map-pin-line"></i>
             {city}
           </span>
           <span className="tour__rating d-flex align-items-center gap-1">
-            <i class="ri-star-fill"></i>
-            <span>{reviews.length}</span>
-            {avgRating}
+            <i className="ri-star-fill"></i>
+            {avgRating === 0 ? null:avgRating}
+            {totalRating === 0 ? 'Not rated' : <span>({reviews.length})</span>}
           </span>
         </div>
 
         <h5 className="tour__title">
-          <Link to={"/tours/${id}"}>{title}</Link>
+        <Link to={`/tours/${id}`}>{title}</Link>
         </h5>
 
         <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
@@ -36,11 +44,12 @@ const TourCard = ({ tour }) => {
             ${price}
             <span> /per person</span>
           </h5>
-          <button className="btn booking_btn">
-            <Link to={"/tours/${id}"}>Book Now</Link>
+          <button className="btn booking__btn">
+          <Link to={`/tours/${id}`}>Book Now</Link>
           </button>
         </div>
       </CardBody>
+      </Card>
     </div>
   );
 };
